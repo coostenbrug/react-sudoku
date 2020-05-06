f84dd13cee09ef8a739dc85d5ce811c18a13d7ab
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2020-05-06 13:51:42 -0400

    changed stack to class syntax

--- src/utils/Stack.js
+++ src/utils/Stack.js
@@ -1,41 +1,43 @@
-var Stack = function() {
-    this.count = 0;
-    this.storage = {};
-}
-
-Stack.prototype.push = function(value) {
-    this.storage[this.count] = value;
-    this.count++;
-}
-
-Stack.prototype.pop = function() {
-    if (this.count === 0) {
-        return undefined;
+class Stack {
+    constructor() {
+        this.count = 0
+        this.storage = {}
     }
 
-    this.count--;
-    var result = this.storage[this.count];
-    delete this.storage[this.count];
-    return result;
-}
-
-Stack.prototype.size = function() {
-    return this.count;
-}
-
-Stack.prototype.peek = function() {
-    if (this.count === 0) {
-        return undefined;
+    push(value) {
+        this.storage[this.count] = value
+        this.count++;
     }
 
-    return this.storage[this.count-1];
-}
-
-Stack.prototype.clear = function() {
-    for (let i = 0; i < this.count; i++) {
-        delete this.storage[i];
-        this.count = 0;
+    pop() {
+        if (this.count === 0) {
+            return undefined;
+        }
+    
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
     }
+
+    peek() {
+        if (this.count === 0) {
+            return undefined;
+        }
+    
+        return this.storage[this.count-1];
+    }
+    
+    size() {
+        return this.count;
+    }
+
+    clear() {
+        for (let i = 0; i < this.count; i++) {
+            delete this.storage[i];
+            this.count = 0;
+        }
+    }    
 }
 
 export default Stack
