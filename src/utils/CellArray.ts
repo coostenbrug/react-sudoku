f6c765114e52499b58fe6b2a5cf0611926d1b25f
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2020-05-06 14:17:08 -0400

    converted CellArray to ts

---
+++ src/utils/CellArray.ts
@@ -0,0 +1,30 @@
+import { Cell } from "../types/types"
+
+class CellArray extends Array {
+    constructor(data: Cell[][]) { 
+        super(data.length)
+        Object.keys(data).forEach((row,i) => {
+            this[i]= data[i]
+        })
+    }
+
+    queriedCellsAllHaveProperty(query: Function, property: Function) {
+        let allHaveProperty = true
+        this.forEachCell((cell: Cell)=>{
+            if(query(cell)) {
+                if (property(cell)) {allHaveProperty = false}
+            }
+        })
+        return allHaveProperty
+    }
+
+    forEachCell(fnc: Function) {
+        this.forEach((row,i) => {
+            row.forEach((cell: Cell, j: number) => {
+                fnc(cell,i,j)
+            })
+        })
+    }
+}
+
+export default CellArray
